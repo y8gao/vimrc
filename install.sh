4c1bfbd7bf21c2447e4c4b8868ae1270788a3355
y8gao <y8gao@nokia.com>
2016-11-22 09:29:19 +0200

    fix the issue that installing failed if vundle exists, and the incorrect symbolic link

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Download Vundle..."
+if [[ -d ~/.vim/bundle/Vundle.vim ]]; then
+    rm -rf ~/.vim/bundle/Vundle.vim
+fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [[ $? != 0  ]]; then
@@ -9,5 +12,5 @@ if [[ $? != 0  ]]; then
 fi
 
 echo "Make symbolic link ./_vimrc to ~/.vimrc"
-ln -s ./_vimrc ~/.vimrc && vim +PluginInstall
+ln -sf $(pwd)/_vimrc ~/.vimrc && vim +PluginInstall
 
